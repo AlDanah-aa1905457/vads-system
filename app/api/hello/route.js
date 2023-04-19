345a9178f8c38d396284340522c794071432533d
AlDanah-aa1905457 <113343744+AlDanah-aa1905457@users.noreply.github.com>
2023-04-19 20:52:24 +0300

    delete

--- app/api/hello/route.js
+++
@@ -1,5 +0,0 @@
-export default async function handler(req, res) {
-  const message = req.body;
-  console.log(`Received message from Arduino: ${message}`);
-  res.status(200).send("Message received successfully");
-}
